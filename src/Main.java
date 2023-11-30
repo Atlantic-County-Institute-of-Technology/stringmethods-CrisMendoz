a165e3cf6990ce1f697f33dbe17b266eafbf07d5
Cristal <144721367+CrisMendoz@users.noreply.github.com>
2023-11-30 14:21:56 -0500

    check

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        char letter = '';
+        System.out.print(letter + " = " + (int)letter);
+        m
+        //System.out.print((int)letter);
+    }
+}
