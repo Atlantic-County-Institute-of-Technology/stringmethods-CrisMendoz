b201c77ed5d0b82de82b356a3ad0fd0ae396d100
Cristal <144721367+CrisMendoz@users.noreply.github.com>
2023-12-04 14:10:28 -0500

    Update Main.java
    FIGURED OUT SUFF WOOOO

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        char letter = '';
-        System.out.print(letter + " = " + (int)letter);
-        m
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter Ascii: ");
+
+        int Ascii = myObj.nextInt();
+        System.out.print((char)Ascii);
+
+        //char letter = 'a';
+        //int number = 67;
+        //System.out.print(letter + " = " + (int)letter);
         //System.out.print((int)letter);
+        //System.out.print((char)number);
+
+
+
     }
 }
