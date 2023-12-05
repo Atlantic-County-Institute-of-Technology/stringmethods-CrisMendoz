2343188e92588c29f78a52dea4b94ea890b97938
Cristal <144721367+CrisMendoz@users.noreply.github.com>
2023-12-05 14:06:04 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter Ascii: ");
 
-        int Ascii = myObj.nextInt();
-        System.out.print((char)Ascii);
+        boolean convert = true;
+        while (convert) {
+            int Ascii = myObj.nextInt();
+            System.out.print((char) Ascii);
+            if (Ascii == 0) {
+                convert = false;
+            }
+        }
+
+        Scanner Name = new Scanner(System.in);
+        System.out.println("Enter your Name: ");
+
+
+
 
         //char letter = 'a';
         //int number = 67;
         //System.out.print(letter + " = " + (int)letter);
         //System.out.print((int)letter);
         //System.out.print((char)number);
-
-
-
     }
 }
